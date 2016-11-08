f429c42f7bbaed7e8c6bc894432219881668c0b9
Alexander Sääf <alesa601@student.liu.se>
2016-11-08 17:14:39 +0100

    Add friction to physics.rs

--- src/physics.rs
+++ src/physics.rs
@@ -3,6 +3,8 @@ use na::{Vector2, Point2, Norm, FloatPoint};
 use poolball;
 use blackhole;
 
+const FRICTION: f64 = 0.00001;
+
 // Calculates the gravity acceleration from an object with given mass at given distance
 fn gravity_acc(mass: f64, distance: f64) -> f64 {
     // let gravity_constant = 6.673 * (10.0_f32).powf(-11.0);
@@ -27,6 +29,17 @@ pub fn calculate_gravity(blackholes: Vec<blackhole::Blackhole>, ball: poolball::
     }
     return result;
 }
+
+//Calculates the direction and size of the friction acceleration on the given ball
+pub fn friction(poolball: poolball::Poolball) -> Vector2<f64> {
+	if poolball.velocity == Vector2::new(0.0,0.0){
+		return Vector2::new(0.0,0.0);
+	}
+	let ball_direction = poolball.velocity.normalize();
+	let friction = -1.0 * ball_direction * FRICTION; 
+	return friction;
+}
+
 
 // Basic tests for gravity_acc
 #[test]
@@ -82,4 +95,14 @@ fn test_calculate_gravity_reach() {
     let ball = poolball::Poolball::new(Point2::new(1.0, 1.0));
     let acc_vector = calculate_gravity(blackholes, ball);
     assert_eq!(acc_vector, Vector2::new(0.0, 0.0));
+}
+
+#[test]
+fn test_friction() {
+	let mut ball = poolball::Poolball::new(Point2::new(1.0, 1.0));
+	assert_eq!(friction(ball), Vector2::new(0.0,0.0));
+
+	let mut ball2 = poolball::Poolball::new(Point2::new(1.0, 1.0));
+	ball2.velocity = Vector2::new(1.0,0.0);
+	assert_eq!(Vector2::new(-1.0, 0.0) * FRICTION, friction(ball2));
 }
