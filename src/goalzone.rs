a35661ccb6f14c7aced2e269deb1c3ab64f02c56
holmgr <viktor.holmgren@gmail.com>
2016-11-08 16:12:10 +0100

    Implemented basic goalzone

--- src/goalzone.rs
+++ src/goalzone.rs
@@ -1,6 +1,36 @@
-use na::Point2;
+use na::{Point2, FloatPoint};
+use poolball;
 
+// Implements a basic goalzone to be used in each corner of the playing field.
 pub struct Goalzone {
-    pub position: Point2<f32>,
-    pub radius: f32,
+    position: Point2<f64>,
+    radius: f64,
+}
+const RADIUS: f64 = 10.0;
+
+impl Goalzone {
+    // Creates a new goalzone at the given position
+    pub fn new(position: Point2<f64>) -> Goalzone {
+        Goalzone {
+            position: position,
+            radius: RADIUS,
+        }
+    }
+
+    // Returns whether a given poolball is within the goalzone
+    pub fn reachedGoal(&self, ball: &poolball::Poolball) -> bool {
+        self.radius + ball.radius < self.position.distance(&ball.position)
+    }
+}
+
+#[test]
+fn test_reach() {
+    // Inside range, should succeed
+    let ball = poolball::Poolball::new(Point2::new(0.0, 0.0));
+    let goalzone = Goalzone::new(Point2::new(RADIUS, RADIUS));
+    assert!(goalzone.reachedGoal(&ball), true);
+
+    // Outside of range should fail
+    let goalzone = Goalzone::new(Point2::new(2.0*RADIUS, 2.0*RADIUS));
+    assert!(!goalzone.reachedGoal(&ball));
 }
