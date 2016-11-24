3ef4f7a33678a1c5687d4f139d6c7ca708751f3a
holmgr <viktor.holmgren@gmail.com>
2016-11-24 10:58:39 +0100

    Implement drawing of current score

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ extern crate num_traits;
 use piston::window::WindowSettings;
 use piston::event_loop::*;
 use piston::input::*;
-use glutin_window::GlutinWindow as Window;
+use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
-use piston_window::ellipse::circle;
+use graphics::Transformed;
+use std::path::Path;
+use opengl_graphics::glyph_cache::GlyphCache;
+
 use na::{Point2, Vector2};
 
 mod poolball;
@@ -25,18 +28,31 @@ pub struct Game {
     balls: Vec<poolball::Poolball>,
     blackholes: Vec<blackhole::Blackhole>,
     goalzones: Vec<goalzone::Goalzone>,
+    score: i32,
 }
 
 const BALL_SIZE: f64 = 10.0;
 
 impl Game {
-    fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs) {
+    fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs, cache: &mut GlyphCache) {
         use graphics::*;
 
         const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
         gl.draw(args.viewport(), |c, g| {
             // Clear the screen.
             clear(BLACK, g);
+        });
+
+        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+
+        // Draw scoe
+        gl.draw(args.viewport(), |c, gl| {
+
+            let trans = c.transform
+                .trans(0.46 * (args.width as f64), 0.02 * (args.height as f64));
+            let score_str = format!("Score: {}", self.score);
+            text::Text::new_color(WHITE, 18)
+                .draw(score_str.as_str(), cache, &c.draw_state, trans, gl);
         });
 
         const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 0.4];
@@ -47,7 +63,6 @@ impl Game {
             goalzone.render(green_ellipse, args, gl);
         }
 
-        const WHITE: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
         const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
 
         let red_ellipse = Ellipse::new(RED);
@@ -76,7 +91,7 @@ fn main() {
     let opengl = OpenGL::V3_2;
 
     // Create an Glutin window.
-    let mut window: Window = WindowSettings::new("spinning-square", [800, 800])
+    let mut window: GlutinWindow = WindowSettings::new("spinning-square", [800, 800])
         .opengl(opengl)
         .exit_on_esc(true)
         .build()
@@ -102,14 +117,18 @@ fn main() {
         balls: balls,
         blackholes: Vec::new(),
         goalzones: goalzones,
+        score: 0,
     };
 
     game.cueball.set_velocity(&Vector2::new(0.01, -0.01));
 
+    let font_path = Path::new("assets/FiraSans-Regular.ttf");
+    let ref mut cache = GlyphCache::new(font_path).unwrap();
+
     let mut events = window.events();
     while let Some(e) = events.next(&mut window) {
         if let Some(r) = e.render_args() {
-            game.render(&mut gl, &r);
+            game.render(&mut gl, &r, cache);
         }
 
         if let Some(u) = e.update_args() {
